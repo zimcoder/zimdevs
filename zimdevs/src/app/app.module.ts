7f3a13868b5a749635b49b73dfbbed0d53b310b1
Clement Siby <clement.siby@momentum.co.za>
2018-03-12 15:31:59 +0200

    added material design

--- zimdevs/src/app/app.module.ts
+++ zimdevs/src/app/app.module.ts
@@ -1,17 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatToolbarModule, MatButtonModule} from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
+    MatToolbarModule,
+    MatButtonModule,
     AppRoutingModule
   ],
   providers: [],
