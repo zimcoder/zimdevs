2790f691491cc576a53029feb72dc999b809782a
Clement Siby <clement@clementsiby.com>
2018-03-23 11:23:03 +0200

    update for moving to a new machine

---
+++ zimdevs-API/models/Developer.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Developer
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string EmailAddress { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public DateTime RegistrationDate { get; set; }
+    
+}
